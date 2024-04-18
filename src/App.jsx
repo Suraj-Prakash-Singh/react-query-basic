ec571304fb98df8fe7a007453c912ec1310a2ea4
TesnSai <shivamchaudhary8476@gmail.com>
2024-04-18 20:41:40 +0530

    mutation basics in react-query

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,46 @@
-import { useQuery } from "@tanstack/react-query"
+import { useQuery, useMutation} from "@tanstack/react-query"
 
 function App() {
 
   const query = useQuery({
     queryKey: ["todo"],
-    queryFn: () => fetch('https://jsonplaceholder.typicode.com/todos').then((res) => res.json())
+    queryFn: () => fetch('https://jsonplaceholder.typicode.com/posts').then((res) => res.json())
   })
 
-  if(query.error){
+  const dummyPost = {
+    title: 'Random Post title',
+    id: 696969,
+    body: 'This post was created by suraj'
+  }
+
+  const mutation = useMutation({
+    mutationFn: (newPost) => fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: "POST",
+      body: JSON.stringify(newPost),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    }).then((res) => res.json()),
+    onSuccess: 
+  })
+
+  if(query.error || mutation.error){
     return <div>Error ocurred!</div>
   }
   if(query.isLoading){
     return <>Loading</>
   }
   return (
-    <>{query.data.map((todo) => 
+    <>
+    <button onClick={() => mutation.mutate(dummyPost)}>Add Post</button>
+    {query.data.map((post) => 
       <div>
-        <h2>ID: {todo.id}</h2>
-        <p>Title: {todo.title}</p>
+        <h4>ID: {post.id}</h4>
+        <p>Title: {post.title}</p>
+        <p>Description: {post.body}</p>
       </div>)}
     </>
   )
 }
 
-
-// function wait(duration){
-//   return new Promise(resolve => setTimeout(resolve, duration));
-// }
 export default App
