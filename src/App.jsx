8638237666937344f03aff7333d4cc932e52bf8b
TesnSai <shivamchaudhary8476@gmail.com>
2024-04-18 17:02:34 +0530

    basic fetching/query using react-query

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
+import { useQuery } from "@tanstack/react-query"
+
 function App() {
 
+  const query = useQuery({
+    queryKey: ["todo"],
+    queryFn: () => fetch('https://jsonplaceholder.typicode.com/todos').then((res) => res.json())
+  })
+
+  if(query.error){
+    return <div>Error ocurred!</div>
+  }
+  if(query.isLoading){
+    return <>Loading</>
+  }
   return (
-    <>Render Sucess</>
+    <>{query.data.map((todo) => 
+      <div>
+        <h2>ID: {todo.id}</h2>
+        <p>Title: {todo.title}</p>
+      </div>)}
+    </>
   )
 }
 
+
+// function wait(duration){
+//   return new Promise(resolve => setTimeout(resolve, duration));
+// }
 export default App
